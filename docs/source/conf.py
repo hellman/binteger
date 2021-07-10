0d0bff8a30d08129423a2dbab40d12c0314f0328
hellman <hellman@users.noreply.github.com>
2021-07-10 12:46:06 +0200

    parse version from config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,18 +10,19 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import re
 import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../../'))
-
-import importlib.metadata
 
 # -- Project information -----------------------------------------------------
 
 project = 'binteger'
 copyright = '2021, Aleksei Udovenko'
 author = 'Aleksei Udovenko'
-release = importlib.metadata.metadata('binteger')["Version"]
+
+config = open("../../pyproject.toml").read()
+release = re.search(r'version = "(.*?)"', config).group(1)
 
 
 # -- General configuration ---------------------------------------------------
